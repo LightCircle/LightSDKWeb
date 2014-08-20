bebf117e97eb14c4e0ee3c00402dbf02d2ed6f8a
LI LIN <r2space@gmail.com>
2014-08-20 10:39:04 +0800

    添加random方法

--- light.js
+++ light.js
@@ -573,6 +573,10 @@ var light = {
         sFirst: "第一页"
       }
     };
+  },
+
+  randomGUID4: function() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   }
 };
 

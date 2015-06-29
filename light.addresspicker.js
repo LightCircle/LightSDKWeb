71afa00181b725700443d72620abe627e9d56b9a
Sara <fyx1014@hotmail.com>
2015-06-29 22:38:08 +0800

    地区选择的新ui对应＋数据加载对应

--- light.addresspicker.js
+++ light.addresspicker.js
@@ -1,49 +1,49 @@
 (function ($) {
 
-/*
-一、用法示例：
+  /*
+   一、用法示例：
 
-  1.无数据初始化
+   1.无数据初始化
 
-    a.使用默认参数
-    $('#your_div').addresspicker(null);
+   a.使用默认参数
+   $('#your_div').addresspicker(null);
 
-    b.使用自定义的option
-    $('#your_div').addresspicker(null,your_options);
+   b.使用自定义的option
+   $('#your_div').addresspicker(null,your_options);
 
-  2.带数据初始化
+   2.带数据初始化
 
-    a.使用默认参数
-      $('#your_div').addresspicker(your_data);
+   a.使用默认参数
+   $('#your_div').addresspicker(your_data);
 
-    b.使用自定义的option
-      $('#your_div').addresspicker(your_data,your_options);
+   b.使用自定义的option
+   $('#your_div').addresspicker(your_data,your_options);
 
-  3.从控件获取值
+   3.从控件获取值
 
-    $('#your_div').addresspicker();
+   $('#your_div').addresspicker();
 
-二、option说明
+   二、option说明
 
-  {
-    level: 3,              //可选值[2,3] 2-省市选择 3-省市区选择
+   {
+   level: 3,              //可选值[2,3] 2-省市选择 3-省市区选择
 
-    changed: null,         //选择改变时触发的函数
+   changed: null,         //选择改变时触发的函数
 
-    error: null,           //加载错误是处罚的函数
+   error: null,           //加载错误是处罚的函数
 
-    class: 'form-control', //每个select元素的class,可为数组
-                           //当为数组时,数组第一个元素对应第一个select的class,第二个元素对应第二个select的class,第三个同理
+   class: 'form-control', //每个select元素的class,可为数组
+   //当为数组时,数组第一个元素对应第一个select的class,第二个元素对应第二个select的class,第三个同理
 
-    wrap: $('<div>').addClass('col-sm-4')
-                           //每个select的外层元素,可为jQuery对象,可为字符串,或者两者构成的数组
-                           //当为数组时,数组的第一个元素对应第一个select的外层元素,数组的第一个元素对应第一个select的外层元素,第三个同理
-  };
+   wrap: $('<div>').addClass('col-sm-4')
+   //每个select的外层元素,可为jQuery对象,可为字符串,或者两者构成的数组
+   //当为数组时,数组的第一个元素对应第一个select的外层元素,数组的第一个元素对应第一个select的外层元素,第三个同理
+   };
 
-  当不传option或者option中项不全时,空缺的option项为默认值.
-  各个option的默认值见 $.fn.addresspicker.defaults
+   当不传option或者option中项不全时,空缺的option项为默认值.
+   各个option的默认值见 $.fn.addresspicker.defaults
 
-*/
+   */
 
 
 
@@ -84,6 +84,45 @@
 
       var opts = $.extend({}, $.fn.addresspicker.defaults, opt);
 
+      // 数据加载
+      var curData = {};
+
+      if (data) {
+        var target = data._id
+          , parent = data.parent;
+        // 省
+        if (parent == "root") {
+          curData = {
+            province: target ,
+            city    : "",
+            town    : ""
+          }
+          opts.level = 1;
+
+        } else if (data.options && data.options.place) {
+          var grand = data.options.place[parent].parent;
+          // 市
+          if (grand && grand == "root") {
+
+            curData = {
+              province: parent ,
+              city    : target,
+              town    : ""
+            }
+            opts.level = 2;
+
+            // 县
+          } else {
+            curData = {
+              province: grand,
+              city    : parent,
+              town    : target
+            }
+
+          }
+        }
+      }
+
       return this.each(function () {
         $(this).empty();
         for (var i = 0; i < opts.level; i++) {
@@ -93,17 +132,22 @@
           sel.addClass(sel_cls);
 
           var wrap = _.isArray(opts.wrap) ? opts.wrap[i] : opts.wrap;
-          $(this).append(wrap.clone().append(sel));
 
+          if (opts.styled) {
+            wrap = wrap.clone();
+            sel = $('<label>').hide().addClass('select').append(sel).append($('<i>'));
+          }
+
+          $(this).append(wrap.append(sel));
         }
-        init($(this), data, opts);
+        init($(this), curData, opts);
       });
     }
 
   };
 
 
-  function init(div, values, opt) {
+  function init(div, curData, opt) {
 
     var sels = div.find('select');
 
@@ -139,7 +183,7 @@
 
       sel.append($("<option />").html('请选择').val(-1));
       _.each(data.items, function (item) {
-        if (index !== -1 && values && values[index] && values[index].id === item._id) {
+        if (index !== -1 && curData && curData[index] === item._id) {
           sel.append($("<option />").html(item.name).val(item._id).attr('selected', 'selected'));
           target = item._id;
         } else {
@@ -147,6 +191,7 @@
         }
       });
       sel.show();
+      sel.parent().show();
       return target;
     };
 
@@ -171,6 +216,7 @@
       citySel.empty();
       if (code == -1) {
         citySel.hide();
+        citySel.parent().hide();
       } else {
         loadPlace(code, function (result) {
           appendPlace(citySel, result, -1);
@@ -178,6 +224,7 @@
       }
       if (townSel) {
         townSel.hide();
+        townSel.parent().hide();
       }
       if (opt.changed) {
         opt.changed();
@@ -192,6 +239,7 @@
       townSel.empty();
       if (code == -1) {
         townSel.hide();
+        townSel.parent().hide();
       } else {
         loadPlace(code, function (result) {
           appendPlace(townSel, result, -1);
@@ -213,12 +261,16 @@
 
 
   $.fn.addresspicker.defaults = {
-    url:'/place/list',
+    url:'/api/place/list',
     level: 3,
     changed: null,
     error: null,
     class: 'form-control',
-    wrap: $('<div>').addClass('col-sm-4')
+    styled: true,
+    wrap: [
+      $('<div>').addClass('col-sm-4').css('padding', '2px'),
+      $('<div>').addClass('col-sm-4').css('padding', '2px'),
+      $('<div>').addClass('col-sm-4').css('padding', '2px')]
   };
 
 })(jQuery);

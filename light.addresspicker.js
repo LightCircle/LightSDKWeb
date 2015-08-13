e54ba3ddb993a48bd4893b8cd1527c1c6bc25242
fzcs <fzcs@live.cn>
2015-08-13 10:35:07 +0800

    修正地址选择空间

--- light.addresspicker.js
+++ light.addresspicker.js
@@ -84,45 +84,6 @@
 
       var opts = $.extend({}, $.fn.addresspicker.defaults, opt);
 
-      // 数据加载
-      var curData = {};
-
-      if (data) {
-        var target = data._id
-          , parent = data.parent;
-        // 省
-        if (parent == "root") {
-          curData = {
-            province: target ,
-            city    : "",
-            town    : ""
-          }
-          opts.level = 1;
-
-        } else if (data.options && data.options.place) {
-          var grand = data.options.place[parent].parent;
-          // 市
-          if (grand && grand == "root") {
-
-            curData = {
-              province: parent ,
-              city    : target,
-              town    : ""
-            }
-            opts.level = 2;
-
-            // 县
-          } else {
-            curData = {
-              province: grand,
-              city    : parent,
-              town    : target
-            }
-
-          }
-        }
-      }
-
       return this.each(function () {
         $(this).empty();
         for (var i = 0; i < opts.level; i++) {
@@ -132,22 +93,17 @@
           sel.addClass(sel_cls);
 
           var wrap = _.isArray(opts.wrap) ? opts.wrap[i] : opts.wrap;
+          $(this).append(wrap.clone().append(sel));
 
-          if (opts.styled) {
-            wrap = wrap.clone();
-            sel = $('<label>').hide().addClass('select').append(sel).append($('<i>'));
-          }
-
-          $(this).append(wrap.append(sel));
         }
-        init($(this), curData, opts);
+        init($(this), data, opts);
       });
     }
 
   };
 
 
-  function init(div, curData, opt) {
+  function init(div, values, opt) {
 
     var sels = div.find('select');
 
@@ -168,7 +124,6 @@
       light.doget(opt.url, {condition: {parent: parent, valid: 1}, order: "sort"}, function (err, result) {
 
         if (err) {
-          alertify.error("加载错误");
           if (opt.error) {
             opt.error(err);
           }
@@ -184,7 +139,7 @@
 
       sel.append($("<option />").html('请选择').val(-1));
       _.each(data.items, function (item) {
-        if (index !== -1 && curData && curData[index] === item._id) {
+        if (index !== -1 && values && values[index] && values[index].id === item._id) {
           sel.append($("<option />").html(item.name).val(item._id).attr('selected', 'selected'));
           target = item._id;
         } else {
@@ -192,7 +147,6 @@
         }
       });
       sel.show();
-      sel.parent().show();
       return target;
     };
 
@@ -217,7 +171,6 @@
       citySel.empty();
       if (code == -1) {
         citySel.hide();
-        citySel.parent().hide();
       } else {
         loadPlace(code, function (result) {
           appendPlace(citySel, result, -1);
@@ -225,7 +178,6 @@
       }
       if (townSel) {
         townSel.hide();
-        townSel.parent().hide();
       }
       if (opt.changed) {
         opt.changed();
@@ -240,7 +192,6 @@
       townSel.empty();
       if (code == -1) {
         townSel.hide();
-        townSel.parent().hide();
       } else {
         loadPlace(code, function (result) {
           appendPlace(townSel, result, -1);
@@ -267,11 +218,7 @@
     changed: null,
     error: null,
     class: 'form-control',
-    styled: true,
-    wrap: [
-      $('<div>').addClass('col-sm-4').css('padding', '2px'),
-      $('<div>').addClass('col-sm-4').css('padding', '2px'),
-      $('<div>').addClass('col-sm-4').css('padding', '2px')]
+    wrap: $('<div>').addClass('col-sm-4')
   };
 
 })(jQuery);

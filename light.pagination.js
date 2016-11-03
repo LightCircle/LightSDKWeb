6a0fb28d31f015ab99332975d2c643724740d8b7
1850061289 <1850061289@qq.com>
2016-11-03 17:52:52 +0800

    pagination 代码优化

--- light.pagination.js
+++ light.pagination.js
@@ -1,17 +1,9 @@
 /**
- * Created by lwx on 16/10/31.
- */
-/**
+ * Created by lwx on 16/10/28.
  * rowCount [number]  row counts of every page
  * pageSize [number]  Number of rows per increase
- * limit    [number]  current pages
  * totalItems  [number] total items
- * start    [number]  start page
- * canPre   [boolean] if can pre
- * startPage[number]  start page
- * canNext  [boolean] if can next
- * activePage  [number]  clicked page
- * skip     [number]  Load from the first few
+ * display   [string]   if more button shows
  * @param id
  * @param option
  * @returns {*}
@@ -19,8 +11,7 @@
 'use strict';
 
 var React = require('react')
-  , ReactDOM = require('react-dom')
-  , net = require('./light.net');
+  , ReactDOM = require('react-dom');
 
 module.exports = function (id, option) {
   option = option || {};
@@ -32,10 +23,13 @@ module.exports = function (id, option) {
   );
 };
 
+var totalpages;
+
 var Pagination = React.createClass({
   getInitialState: function () {
+    totalpages = Math.ceil(this.props.totalItems / this.props.rowCount);
     return {
-      limit: Math.ceil(this.props.totalItems / this.props.rowCount) > 5 ? 5 : Math.ceil(this.props.totalItems / this.props.rowCount),
+      limit: totalpages > 5 ? 5 : totalpages,
       totalItems: this.props.totalItems,
       start: 1,
       canPre: false,
@@ -51,38 +45,51 @@ var Pagination = React.createClass({
   render: function () {
     return React.DOM.div({},
       React.DOM.div({
-          className: 'more', style: {
-            color: '#72c02c', fontSize: '18px', background: 'none', textAlign: 'center', border: 'solid 1px #e67e22',
-            borderRadius: '4px', padding: '5px 13px', marginBottom: '5px', display: this.props.display
-          }
+          className: 'more',
+          style: {
+            color: '#72c02c', fontSize: '18px', background: 'none',
+            textAlign: 'center', border: 'solid 1px #e67e22', borderRadius: '4px',
+            padding: '5px 13px', marginBottom: '5px', display: this.props.display
+          },
+          onClick:this.click,
+          onMouseOver:this.onmouseover,
+          onMouseOut:this.onmouseout
         },
         React.DOM.i({className: 'fa fa-flickr', style: {marginRight: '8px'}}), '加载更多'
       ),
       React.DOM.div({className: 'pull-right'},
-        React.DOM.ul(
-          {className: 'pagination', style: {marginBottom: '0px', marginTop: '0px'}}
-          , this.canPrev()
-          , this.page()
-          , this.canNext()
+        React.DOM.ul({className: 'pagination', style: {marginBottom: '0px', marginTop: '0px'}},
+          this.canPrev(),
+          this.page(),
+          this.canNext()
         )
       )
     )
   },
 
-
   page: function () {
-    var remainder = Math.ceil((this.state.totalItems - (this.state.startPage - 1) * this.state.rowCount) / this.state.rowCount)
-      , limit = remainder > 5 ? 5 : remainder;
+    var loadedItems = (this.state.startPage - 1) * this.state.rowCount;
+    var remainder = Math.ceil((this.state.totalItems - loadedItems) / this.state.rowCount),
+      limit = remainder > 5 ? 5 : remainder;
     this.setState({limit: limit});
     var pages = [];
     for (var i = this.state.start; i < this.state.start + this.state.limit; i++) {
       if (i == this.state.activePage) {
         pages.push(React.DOM.li({className: 'active'},
-          React.DOM.a({href: 'javascript:void(0)', style: {padding: '4px 9px'}, 'data-activePage': i}, i)))
-      }
-      else {
+          React.DOM.a({
+            href: 'javascript:void(0)',
+            style: {padding: '4px 9px'},
+            'data-activePage': i,
+            onClick: this.changePage
+          }, i)))
+      } else {
         pages.push(React.DOM.li({},
-          React.DOM.a({href: 'javascript:void(0)', style: {padding: '4px 9px'}, 'data-activePage': i}, i)))
+          React.DOM.a({
+            href: 'javascript:void(0)',
+            style: {padding: '4px 9px'},
+            'data-activePage': i,
+            onClick: this.changePage
+          }, i)))
       }
     }
     return pages;
@@ -96,109 +103,99 @@ var Pagination = React.createClass({
             className: 'pre',
             href: 'javascript:void(0)',
             style: {padding: '4px 9px'},
-            'data-activePage': 'prev'
+            'data-activePage': 'prev',
+            onClick: this.changePage
           },
-          React.DOM.i({className: 'fa fa-angle-double-left'}))
+          React.DOM.i({
+            className: 'fa fa-angle-double-left',
+            'data-activePage': 'prev',
+            onClick: this.changePage
+          }))
       )
-    }
-    else {
+    } else {
       this.setState({canPre: false});
     }
   },
 
   canNext: function () {
-    if ((this.state.startPage + this.state.limit - 1 < Math.ceil(this.state.totalItems / this.state.rowCount)) && (this.state.limit >= 5)) {
+    if ((this.state.startPage + this.state.limit - 1 < totalpages) && (this.state.limit >= 5)) {
       this.setState({canNext: true});
       return React.DOM.li({},
         React.DOM.a({
             className: 'next',
             href: 'javascript:void(0)',
             style: {padding: '4px 9px'},
-            'data-activePage': 'next'
+            'data-activePage': 'next',
+            onClick: this.changePage
           },
-          React.DOM.i({className: 'fa fa-angle-double-right'}))
+          React.DOM.i({
+            className: 'fa fa-angle-double-right',
+            'data-activePage': 'next',
+            onClick: this.changePage
+          }))
       )
-    }
-    else {
+    } else {
       this.setState({canNext: false});
     }
   },
 
   paginationMore: function () {
     if (this.state.totalItems <= this.state.skip + this.state.pageSize) {
-      $('.more').addClass("hide");
+      $('.more').addClass('hide');
     } else {
-      $('.more').removeClass("hide");
-      this.setState({
-        skip: this.state.skip += this.state.pageSize
-      }, this.showMore(this.state.skip));
+      $('.more').removeClass('hide');
+      this.setState({skip: this.state.skip += this.state.pageSize},
+        this.showMore(this.state.skip));
     }
   },
 
-  componentDidMount: function () {
-    var node = $(ReactDOM.findDOMNode(this));
-    node.unbind("click").on('click', 'a', function (event) {
-      if (event.target.tagName == 'A') {
-        var activePage = $(event.target).attr("data-activePage");
+  changePage: function () {
+    var activePage = $(event.target).attr('data-activePage');
+    if (activePage == 'prev') {
+      if (this.state.startPage == 1) {
+        return false;
+      }
+      if (this.state.startPage - 5 < 1) {
+        this.show(0);
+        this.setState({startPage: 1, activePage: 1});
       } else {
-        var activePage = $(event.target.parentNode).attr("data-activePage");
+        var prevStart = this.state.startPage - 5;
+        this.show((prevStart - 1) * this.state.rowCount);
+        this.setState({
+          startPage: prevStart, activePage: prevStart, start: prevStart
+        });
       }
-
-      if (activePage == "prev") {
-        if (this.state.startPage == 1) {
-          return false;
-        } else {
-          if (this.state.startPage - 5 < 1) {
-            this.show(0);
-            this.setState({startPage: 1, activePage: 1})
-          }
-          else {
-            this.show((this.state.startPage - 5 - 1) * this.state.rowCount);
-            this.setState({
-              startPage: this.state.startPage - 5
-              , activePage: this.state.startPage - 5
-              , start: this.state.startPage - 5
-            })
-          }
-        }
-      } else if (activePage == "next") {
-        if (Math.ceil((this.state.totalItems - (this.state.startPage - 1) * this.state.rowCount) / this.state.rowCount) > 5) {
-          this.show((this.state.startPage + 5 - 1) * this.state.rowCount);
-          this.setState({
-            startPage: this.state.startPage + 5
-            , activePage: this.state.startPage + 5
-            , start: this.state.startPage + 5
-          })
-        }
-        else {
-          return false;
-        }
+    } else if (activePage == 'next') {
+      var loadedItems = (this.state.startPage - 1) * this.state.rowCount;
+      if (Math.ceil((this.state.totalItems - loadedItems) / this.state.rowCount) > 5) {
+        var nextStart = this.state.startPage + 5;
+        this.show((nextStart - 1) * this.state.rowCount);
+        this.setState({startPage: nextStart, activePage: nextStart, start: nextStart});
       }
-      else {
-        this.show((activePage - 1) * this.state.rowCount);
-        this.setState({activePage: activePage});
-      }
-    }.bind(this));
-
-    node.on('click', 'div:first-child', function () {
-      this.paginationMore();
-    }.bind(this));
-
-    node.on('mouseover', 'div:first-child', function (event) {
-      $(event.target).css({backgroundColor: '#e67e22'});
-    });
-
-    node.on('mouseout', 'div:first-child', function (event) {
-      $(event.target).css({backgroundColor: '#fff'});
-    });
+      return false;
+    } else {
+      this.show((activePage - 1) * this.state.rowCount);
+      this.setState({activePage: activePage});
+    }
   },
 
-  show: function (activePage) {
-    this.props.show(activePage);
+  click:function(){
+    this.paginationMore();
+  },
+
+  onmouseover:function(){
+    $(event.target).css({backgroundColor: '#e67e22'});
+  },
+
+  onmouseout:function(){
+    $(event.target).css({backgroundColor: '#fff'});
+  },
+
+  show: function (skip) {
+    this.props.show(skip);
   },
 
   showMore: function (skip) {
     this.props.showMore(skip)
   }
-
 });
